369c33e8288a6882c851ea8b66301f516a0566d1
MuratÇamurlu <mcamurlu1923@gmail.com>
2023-02-16 15:18:43 +0300

    home

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -6,19 +6,17 @@ import Slider from "../../components/slider/Slider";
 import Spinner from "../../components/spinner/Spinner";
 const HomePage = () => {
   const [news, setNews] = useState([]);
-  const [loading, setLoading] = useState(false);
+
   const [pageNumber, setPageNumber] = useState(0);
 
   // const API_KEY = "676f017549224f488970f1835f9db971";
   const API_KEY = "242fb770517b456dac640c348e0fed1c";
   const url = `https://newsapi.org/v2/top-headlines?country=tr&apiKey=${API_KEY}`;
   const getNews = () => {
-    setLoading(true);
     fetch(url)
       .then((res) => res.json())
       .then((data) => setNews(data.articles))
-      .catch((err) => console.log(err))
-      .finally(() => setLoading(false));
+      .catch((err) => console.log(err));
   };
 
   useEffect(() => {
@@ -40,7 +38,6 @@ const HomePage = () => {
         <Slider sliderNews={sliderNews} />
       </div>
       <div className={style.news}>
-        {loading && <Spinner />}
         {displayNews.map((item, index) => (
           <NewsCard key={index} {...item} />
         ))}
